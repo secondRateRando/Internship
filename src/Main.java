9b81b1baeaac7ea75a5ae19c3176442be09387fc
Rishabh Jain <rishabhjain1217@gmail.com>
2019-05-20 08:45:44 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Rishabh Jain AKA CodeGod on 05 20, 2019 at 8:30 AM
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello2");
+    }
+}
